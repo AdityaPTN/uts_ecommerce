a77f44a9e0317a53685bc004c812ee67cbdea0ee
aditya <adityasanuzie204@gmail.com>
2022-11-04 07:35:46 +0700

    client

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose')
 const session = require('express-session')
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT;
-
-
 
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('err', (err)=> console.log(err));
 db.once('open', ()=> console.log('connect'));
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static('uploads'));
--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session')
 const Product = require('../models/products.models');
+const app = express();
+
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
+}));
+
+const isProductInCart = (cart, id)=> {
+    for(let i=0; i<cart.length;i++){
+        if(cart[i].id == id){
+            return true;
+        }
+    }
+    return false;
+}
+
+const calculateTotal = (cart,req)=>{
+    total = 0;
+    for(let i=0;i<cart.length;i++){
+        total = total + (cart[i].price*cart[i].quantity);
+    }
+    req.session.total = total;
+    return total;
+}
 
 router.get('/', (req,res)=>{
     Product.find().exec((err, products)=>{
@@ -9,13 +35,13 @@ router.get('/', (req,res)=>{
         } else {
             res.render('client/index', {
                 style: "homepage",
-                products: products,
+                barang: products,
             })
         }
     })
 })
 
-router.get('/:id', (req,res)=>{
+router.get('/shop/:id', (req,res)=>{
     let id = req.params.id;
     Product.findById(id, (err,product)=>{
         if(err){
@@ -31,6 +57,55 @@ router.get('/:id', (req,res)=>{
             }
         }
     })
+})
+
+router.get('/cart', (req,res)=>{
+    var cart = req.session.cart;
+    var total = req.session.total;
+
+    res.render('client/cart', {
+        cart: cart,
+        total: total,
+    })
+})
+
+router.post('/add_to_cart', (req,res)=>{
+    let id = req.body.id;
+    let name = req.body.name;
+    let price = req.body.price;
+    let quantity = req.body.quantity;
+    let image = req.body.image;
+    let product = {id:id, name:name, price:price, quantity:quantity, image:image};
+
+    if(req.session.cart){
+        var cart = req.session.cart;
+
+        if(!isProductInCart()){
+            cart.push(product);
+        }
+
+    }else{
+        req.session.cart = [product];
+        var cart = req.session.cart;
+    }
+
+    calculateTotal(cart,req);
+
+    res.redirect('/cart');
+})
+
+router.post('/cart/remove_product', (req,res)=>{
+    var id = req.body.id;
+    var cart = req.session.cart;
+
+    for(let i=0;i<cart.length;i++){
+        if(cart[i].id == id){
+            cart.splice(cart.indexOf(i),1);
+        }
+    }
+
+    calculateTotal(cart, req);
+    res.redirect('/cart')
 })
 
 module.exports = router;
